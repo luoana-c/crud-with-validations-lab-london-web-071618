5321da6bec61796b1e06011f14554d2fc963d216
github username <github email address>
2018-08-14 11:47:50 +0100

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,12 @@
+class Song < ActiveRecord::Base
+  validates :title, presence: true, uniqueness: {scope: [:artist_name, :release_year], message: "Cannot be repeated by the same artist in the same year"}
+  validates :released, inclusion: {in: [true, false]}
+  with_options if: :released? do |song|
+    song.validates :release_year, presence: true
+    song.validates :release_year, numericality: {less_than_or_equal_to: Date.today.year}
+  end
+
+  def released?
+    released
+  end
+end
